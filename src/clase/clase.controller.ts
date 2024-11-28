ec87ced34a95297d7fc08e5c5f1881f8e9beb7bc
Gabriela <g.garcias2@uniandes.edu.co>
2024-11-28 10:40:44 -0500

    fin

--- src/clase/clase.controller.ts
+++ src/clase/clase.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
 
-@Controller('clase')
-export class ClaseController {}
+import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors/business-errors.interceptor';
+import { ClaseDto } from './clase.dto/clase.dto';
+import { ClaseEntity } from './clase.entity/clase.entity';
+import { plainToInstance } from 'class-transformer';
+import { ClaseService } from './clase.service';
+
+@Controller('clases')
+@UseInterceptors(BusinessErrorsInterceptor)
+export class ClaseController {
+  constructor(private readonly claseService: ClaseService) {}
+  @Post()
+  async create(@Body() claseDto: ClaseDto) {
+    const clase: ClaseEntity = plainToInstance(ClaseEntity, claseDto);
+    return await this.claseService.create(clase);
+  }
+
+  @Get(':claseId')
+  async findOne(@Param('claseId') claseId: string) {
+    return await this.claseService.findOne(claseId);
+  }
+}
--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
+import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors/business-errors.interceptor';
+import { UsuarioService } from './usuario.service';
+import { UsuarioDto } from './usuario.dto/usuario.dto';
+import { UsuarioEntity } from './usuario.entity/usuario.entity';
+import { plainToInstance } from 'class-transformer';
 
 @Controller('usuario')
-export class UsuarioController {}
+@UseInterceptors(BusinessErrorsInterceptor)
+export class UsuarioController {
+  constructor(private readonly usuarioService: UsuarioService) {}
+
+  @Post()
+  async create(@Body() usuarioDto: UsuarioDto) {
+    const usuario: UsuarioEntity = plainToInstance(UsuarioEntity, usuarioDto);
+    return await this.usuarioService.crearUsuario(usuario);
+  }
+
+  @Get(':usuarioId')
+  async findOne(@Param('usuarioId') usuarioId: string) {
+    return await this.usuarioService.findOne(usuarioId);
+  }
+
+  @Delete(':usuarioId')
+  @HttpCode(204)
+  async delete(@Param('usuarioId') usuarioId: string) {
+    return await this.usuarioService.deleteUsuario(usuarioId);
+  }
+}
