0163fb9744ee879d9330cc5452d81b93c40422b0
Gabriela <g.garcias2@uniandes.edu.co>
2024-11-28 21:20:00 -0500

    controladores

--- src/clase-bono/clase-bono.controller.ts
+++ src/clase-bono/clase-bono.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
+import { ClaseBonoService } from 'src/clase-bono/clase-bono.service';
+import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors/business-errors.interceptor';
 
-@Controller('clase-bono')
-export class ClaseBonoController {}
+@Controller('clases/:cod/bonos')
+@UseInterceptors(BusinessErrorsInterceptor)
+export class ClaseBonoController {
+  constructor(private readonly claseBonoService: ClaseBonoService) {}
+
+  @Get()
+  async findBonosByClase(@Param('cod') cod: string) {
+    return await this.claseBonoService.findBonosByClase(cod);
+  }
+}
--- src/usuario-bono/usuario-bono.controller.ts
+++ src/usuario-bono/usuario-bono.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
+import { UsuarioBonoService } from 'src/Usuario-bono/Usuario-bono.service';
+import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors/business-errors.interceptor';
 
-@Controller('usuario-bono')
-export class UsuarioBonoController {}
+@Controller('usuarios/:usuarioId/bonos')
+@UseInterceptors(BusinessErrorsInterceptor)
+export class UsuarioBonoController {
+  constructor(private readonly usuarioBonoService: UsuarioBonoService) {}
+
+  @Get()
+  async findBonosByUsuario(@Param('usuarioId') usuarioId: string) {
+    return await this.usuarioBonoService.findBonosByUsuario(usuarioId);
+  }
+}
